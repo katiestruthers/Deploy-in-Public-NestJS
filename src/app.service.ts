68789d07f2183e41a09335a8344777080aac0cb9
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2025-06-08 14:17:48 -0500

    Update HelloWorld message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Attempt to deploy 3';
+    return 'Hello World! This was automatically deployed from GitHub Actions.';
   }
 }
