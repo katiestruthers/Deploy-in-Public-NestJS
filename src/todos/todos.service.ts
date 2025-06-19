161c0911f9de19d98c08540dbce7e1551e874837
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2025-06-18 21:29:55 -0500

    Setup to-do list CRUD app

---
+++ src/todos/todos.service.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { TodoEntity } from './todos.entity';
+import { TodoDTO } from './todos.controller';
+
+@Injectable()
+export class TodosService {
+  private todoRepository;
+
+  constructor(private datasource: DataSource) {
+    this.todoRepository = this.datasource.getRepository(TodoEntity);
+  }
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async create(todo: TodoDTO) {
+    await this.todoRepository.save(todo);
+    return await this.findAll();
+  }
+
+  async update(id: number, todo: TodoDTO) {
+    await this.todoRepository.update(id, todo);
+    return await this.findAll();
+  }
+
+  async delete(id: number) {
+    await this.todoRepository.delete(id);
+    return await this.findAll();
+  }
+}
